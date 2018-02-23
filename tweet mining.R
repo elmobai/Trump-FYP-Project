919abd2a2d1d0a7b0188022f297dea55876770f6
Emlyn <Ego.emlyn@gmail.com>
2018-02-23 19:08:35 +0000

    update fri
    update

--- tweet mining.R
+++ tweet mining.R
@@ -1,20 +1,26 @@
 
-require(twitteR)
-require(RCurl)
-
+#make sure packages below are installed 
+library(twitteR)
+library(RCurl)
 consumer_key <-	'RzthYgmbqYcwKnEa13RWmWkFA'
 consumer_secret <- 'myTxG4Mgx7yjxqTmm6C8aVdiz10SCgZ2czDLt0QQTt2picsESY'
 access_token <- '116297351-goKlUCOvv4lsqnw7bO5G3KK4Ndd1uR4IYyUcylej'
 access_secret <- 'oJoqoowUxPZ3EX3kfDsEJdft2lpNH0rZ1lYyD7TIjk0mR'
-
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
-sink("output.txt")
-userTimeline('realdonaldtrump',n=3199) # tweets from a user
 
-#homeTimeline (n=15) # get tweets from home timeline
+tweets1 <- userTimeline("realDonaldTrump", n = 3200)
+(n.tweet <- length(tweets))
 
-#mentions (n=15) # get your tweets that were retweeted
+tweets[1:3]
+# Converting these reults to a dataframe
+tweets1.pr <- twListToDF(tweets1)
 
-favs <- favorites("r_programming", n =10) # tweets a user has favorited
+tweets1.pr
 
-sink()
+#data clensing process 
+
+library(tm) 
+library(stringr)
+myCorpus <- Corpus(VectorSource(tweets1.pr$text)) 
+# convert all characters to lower case 
+myCorpus <- tm_map(myCorpus, content_transformer(str_to_lower))
