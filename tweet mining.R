98b8e853838e1367bc01c3887e253c807d9f29fb
elmobai <ego.emlyn@gmail.com>
2018-02-24 16:09:32 +0000

    sat update in trello
    ""

--- tweet mining.R
+++ tweet mining.R
@@ -8,19 +8,22 @@ access_token <- '116297351-goKlUCOvv4lsqnw7bO5G3KK4Ndd1uR4IYyUcylej'
 access_secret <- 'oJoqoowUxPZ3EX3kfDsEJdft2lpNH0rZ1lYyD7TIjk0mR'
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
-tweets1 <- userTimeline("realDonaldTrump", n = 3200)
+tweets <- userTimeline("realDonaldTrump", n = 3199)
 (n.tweet <- length(tweets))
 
 tweets[1:3]
 # Converting these reults to a dataframe
-tweets1.pr <- twListToDF(tweets1)
+tweets.pr <- twListToDF(tweets)
 
-tweets1.pr
+tweets.pr
 
 #data clensing process 
 
 library(tm) 
 library(stringr)
-myCorpus <- Corpus(VectorSource(tweets1.pr$text)) 
+myCorpus <- Corpus(VectorSource(tweets.pr$text)) 
 # convert all characters to lower case 
-myCorpus <- tm_map(myCorpus, content_transformer(str_to_lower))
+myCorpus <- tm_map(myCorpus, content_transformer(tolower))
+#remove webaddresses 
+removeURL <- function(x) gsub("http[^[:space:]]*", "", x)
+myCorpus <- tm_map(myCorpus, content_transformer(removeURL))
