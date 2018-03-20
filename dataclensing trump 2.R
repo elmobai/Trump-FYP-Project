cac0b2184427f8fc05c696ed874a51beaff29cbf
elmobai <ego.emlyn@gmail.com>
2018-03-20 18:49:23 +0000

    tues update
    tues

--- dataclensing trump 2.R
+++ dataclensing trump 2.R
@@ -4,10 +4,25 @@
 library(bitops)
 library(RCurl)
 library(twitteR)
+library(tm) 
+library(stringr)
 
 
-dataset[2:3]
+trumptweets[2:2]
 # Conver to dataframe
 dataset <- twListToDF(dataset)
 unclass(dataset("#dataset", n=2600)[[1]])
 #ldply(searchTwitter("#rstats", n=100), text)
+
+
+#data clensing process 
+trumptweetsCorpus <- VectorSource(trumptweets) 
+#volcorpus
+trumptweetsVolCorpus <- VCorpus(trumptweets) 
+
+# convert all characters to lower case 
+tm_map (trumptweetsVolCorpus, tolower )
+myCorpus <- tm_map(myCorpus, content_transformer(tolower))
+#remove webaddresses 
+removeURL <- function(x) gsub("http[^[:space:]]*", "", x)
+myCorpus <- tm_map(myCorpus, content_transformer(removeURL))
